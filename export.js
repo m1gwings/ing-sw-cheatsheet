8f2bffc3d49e3ed50ab5970f83149442c2d5fe6d
Cristiano Migali <cristiano.migali@gmail.com>
2022-12-11 13:07:28 +0100

    Setup iniziale per esportare correttamente Inizializzato il package npm. export.js sfrutta "marpit" e "puppeter" per esportare le slide in pdf. (formato A4, landscape).

---
+++ export.js
@@ -0,0 +1,51 @@
+import { Marpit } from '@marp-team/marpit'
+import markdownItContainer from 'markdown-it-container'
+import fs from 'fs'
+import puppeteer from 'puppeteer'
+
+const percorsoFileMarkdown = 'cheatsheet.md'
+const percorsoFileStile = 'style.css'
+const percorsoFileOutput = 'cheatsheet.pdf'
+
+const marpit = new Marpit().use(markdownItContainer, 'columns')
+
+const tema = fs.readFileSync(percorsoFileStile, 'utf-8')
+marpit.themeSet.default = marpit.themeSet.add(tema)
+
+const markdown = fs.readFileSync(percorsoFileMarkdown, 'utf-8')
+
+const { html, css } = marpit.render(markdown)
+
+const fileHTML = `
+<!DOCTYPE html>
+<head>
+    <style>
+        .marpit {
+            width: fit-content;
+            height: fit-content;
+        }
+    </style>
+</head>
+<html style="margin: 0mm; padding: 0mm; height: fit-content; width: fit-content;">
+    <body style="margin: 0mm; padding: 0mm; height: fit-content; width: fit-content;">
+        <style>${css}</style>
+        ${html}
+    </body>
+</html>
+`
+
+puppeteer.launch().then(browser => {
+    browser.newPage().then(pagina => {
+        pagina.setContent(fileHTML, { waitUntil: 'domcontentloaded' }).then(() => {
+            pagina.emulateMediaType('screen').then(() => {
+                pagina.pdf({
+                    path: percorsoFileOutput,
+                    format: 'A4',
+                    landscape: true,
+                }).then(pdf => {
+                    browser.close()
+                })
+            })
+        })
+    })
+})
